96e2eaebb74efd4ba4821a739afaf14b345c5995
canna88 <97223496+canna88@users.noreply.github.com>
2023-09-21 07:18:59 +0200

    third-commit

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 const accessKey = "28jmzXXtV1tHdDkUgVSpV44dBtQBV4wfrevh04FubOxgqBGfZhnDSpgl";
 const link = "https://api.pexels.com/v1/search?query=";
-const headerAuthorization = {headers: {Authorization: `${accessKey}`}}
-
-
+const headerAuthorization = { headers: { Authorization: `${accessKey}` } };
+let page = 1;
 
 function loadImages(category) {
-  fetch(`${link}${category}`, headerAuthorization)
+  fetch(`${link}${category}&page=${page}`, headerAuthorization)
     .then((response) => response.json())
 
     .then(imagesExtract)
@@ -15,18 +14,61 @@ function loadImages(category) {
     });
 }
 
-// const searchButton = document.getElementById('button-search')
-
-// searchButton.addEventListener("click", function() {
-//     const inputText = document.querySelector('.form-control').value;
-//     loadImages(inputText);
-//     console.log(inputText);
-//   });
-
 function imagesExtract(object) {
-    return console.log(object.photos[0].photographer);
+    
+  const results = object.photos;
+  
+  // Seleziona la sezione delle card
+  const resultsSection = document.querySelector(".search-results");
+  // Ripulisco la sezione al click
+  if (page === 1) {
+    resultsSection.innerHTML = "";
+  } 
+
+  results.forEach((result) => {
+    // Creo la card
+    const card = document.createElement("div");
+    card.classList.add("card");
+
+    const cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
+
+    const cardImage = document.createElement("img");
+    cardImage.classList.add("card-img-top");
+    cardImage.src = result.src.small;
+
+    const cardText = document.createElement("p");
+    cardText.classList.add("card-text");
+    const cardSentence = result.alt;
+    cardText.innerText = cardSentence;
+
+    resultsSection.appendChild(card);
+    card.appendChild(cardImage);
+    card.appendChild(cardBody);
+    cardBody.appendChild(cardText);
+  });
+
+  page += 1
+  if (page > 1 ) {
+    showMoreButton.style.display = "block"
+  }
 }
 
-const results = loadImages("cats")
+const searchButton = document.getElementById("button-search");
+const showMoreButton = document.getElementById("show-more-button");
 
+searchButton.addEventListener("click", function () {
+  page = 1;
+  const inputText = document.querySelector("#search-input").value;
+  console.log(inputText);
+  loadImages(inputText);
+});
 
+showMoreButton.addEventListener("click", function () {
+    const inputText = document.querySelector("#search-input").value;
+    console.log(inputText);
+    loadImages(inputText);
+  });
+  
+
+// const results = loadImages("cats")
